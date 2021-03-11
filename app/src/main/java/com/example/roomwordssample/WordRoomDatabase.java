d2a65e745a6abdbce96faf9c649fe57e31e655c3
sifernj <sifernj@unirioja.es>
2021-03-11 23:23:42 +0100

    Parte B - Terminado

--- app/src/main/java/com/example/roomwordssample/WordRoomDatabase.java
+++ app/src/main/java/com/example/roomwordssample/WordRoomDatabase.java
@@ -45,7 +45,10 @@ public abstract class WordRoomDatabase extends RoomDatabase {
     private static class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
 
         private final WordDao mDao;
-        String[] words = {"dolphin", "crocodile", "cobra"};
+
+        // Initial data set
+        private static String [] words = {"dolphin", "crocodile", "cobra", "elephant", "goldfish",
+                "tiger", "snake"};
 
         PopulateDbAsync(WordRoomDatabase db) {
             mDao = db.wordDao();
@@ -53,13 +56,12 @@ public abstract class WordRoomDatabase extends RoomDatabase {
 
         @Override
         protected Void doInBackground(final Void... params) {
-            // Start the app with a clean database every time.
-            // Not needed if you only populate on creation.
-            mDao.deleteAll();
-
-            for( int i = 0; i <= words.length - 1; i++) {
-                Word word = new Word(words[i]);
-                mDao.insert(word);
+            // If we have no words, then create the initial list of words
+            if (mDao.getAnyWord().length < 1) {
+                for (int i = 0; i <= words.length - 1; i++) {
+                    Word word = new Word(words[i]);
+                    mDao.insert(word);
+                }
             }
             return null;
         }
